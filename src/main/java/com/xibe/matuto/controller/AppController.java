7457f5f174793667d70dbdbace604c28f059ebfc
Moacir Viana <36387066+moacirviana@users.noreply.github.com>
2023-10-16 09:59:53 -0400

    Update AppController.java
    ajustes e melhorias

--- src/main/java/com/xibe/matuto/controller/AppController.java
+++ src/main/java/com/xibe/matuto/controller/AppController.java
@@ -3,6 +3,13 @@ package com.xibe.matuto.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/home")
 public class AppController {
-
+	
+	@GetMapping
+	public ResponseEntity<StandardError> home(HttpServletRequest request){
+    	StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.OK.value(), "Nenhum", "Bem vindo, projeto spring demo", request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.OK).body(err);    	
+    }
+	
 }
